704e3ce1b3c4e280d68dd8e185a72b3054143d96
O'Donnell.Ryan <O'Donnell.Ryan@SEMS-259-10.SEMS259.LAB>
2018-03-07 16:28:25 -0800

    completed FQ,CC

--- src/ChangeCalculator.java
+++ src/ChangeCalculator.java
@@ -14,17 +14,26 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
-		// Convert their answer to an int using Integer.parseInt()
-
+String nick = JOptionPane.showInputDialog("How many nickles?");
+		
+double n = Integer.parseInt(nick);
 		// Ask the user how many dimes they have, and convert their answer
+String dime = JOptionPane.showInputDialog("How many dimes?");
 
+double d = Integer.parseInt(dime);
 		// Ask the user how many quarters they have, and convert their answer
+String quaters = JOptionPane.showInputDialog("How many quarters?");
 
+double q = Integer.parseInt(quaters);
 		// Calculate how much money the user has and save it in a double variable 
-
+double a = n/20+d/10+q/4;
 		// Tell the user how much money they have
-
+if (a<1) {
+	JOptionPane.showMessageDialog(null, "you have "+(a*100)+" cents");
+}
+else if (a>1) {
+	JOptionPane.showMessageDialog(null, "you have "+a+" dollars");
+}
 	}
 }
 
--- src/FruitQuiz.java
+++ src/FruitQuiz.java
@@ -9,11 +9,15 @@ import java.awt.event.KeyEvent;
 import javax.swing.JApplet;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 public class FruitQuiz extends KeyAdapter {
 
 	void makeQuestions() {
 		question1 = new JLabel("<html>Which is not a real fruit? <br> A: Dragon Fruit <br> B: Durian <br> C: Crazyberry</html>");
+		question2 = new JLabel("<html>Which is not a real thing? <br> A: code <br> B: ryan <br> C: matrix</html>");
+		question3 = new JLabel("<html>Which is not a toy to toy with? <br> A: boy <br> B: not this one <br> C: girl</html>");
+		question4 = new JLabel("<html>Which is a real fruit? <br> A: oaotmeal <br> B: Durian <br> C: Crainberry</html>");
 		// 11. Make another question called "question2".  Use question1 above as a guide.
 	}
 
@@ -21,26 +25,84 @@ public class FruitQuiz extends KeyAdapter {
 	public void keyPressed(KeyEvent arg0) {
 		int keyCode = arg0.getKeyCode();
 		// 1. Print out the key code variable
+System.out.println(keyCode);
+		// 14. Repeat steps 11, 12, and 13 for question3 and question4 - 
+		// IMPORTANT: The questions must be in reverse order from top to bottom to work properly
+if (question4.isShowing()) {
+	// 3. If they selected the right fruit, do steps 4 and 7
+	if (keyCode==66) {
+		
+	
+		// 4. Call the correct() method
+		correct();
+		// 7. Use the nextQuestion() method to go to question2
+	new Thread(()-> JOptionPane.showMessageDialog(null, "", "YOU CHEATED CLOSING PROGRAM", JOptionPane.ERROR_MESSAGE) ).start();
+	try {
+		Thread.sleep(3000);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	System.exit(0);
+	}
+	// 8. else (if they touched something else)
+	else  {
+	incorrect();	
+	} 
+		// 9. Call the incorrect() method
 
-		// 2. Make 3 int variables that hold the key codes for A, b, and C
-		
-		// 14. Repeat steps 11, 12, and 13 for question3 and question4 - IMPORTANT: The questions must be in reverse order from top to bottom to work properly
-		
+}
 		// 12. If question2 is showing,
-			
-			// 13. check if it is right or wrong like you did for question1
+if (question3.isShowing()) {
+	// 3. If they selected the right fruit, do steps 4 and 7
+	if (keyCode==67) {
 		
+	
+		// 4. Call the correct() method
+		correct();
+		// 7. Use the nextQuestion() method to go to question2
+	nextQuestion(question4);
+	}
+	// 8. else (if they touched something else)
+	else  {
+	incorrect();	
+	} 
+		// 9. Call the incorrect() method
+
+}
+			// 13. check if it is right or wrong like you did for question1
+if (question2.isShowing()) {
+	// 3. If they selected the right fruit, do steps 4 and 7
+	if (keyCode==67) {
+		
+	
+		// 4. Call the correct() method
+		correct();
+		// 7. Use the nextQuestion() method to go to question2
+	nextQuestion(question3);
+	}
+	// 8. else (if they touched something else)
+	else  {
+	incorrect();	
+	} 
+		// 9. Call the incorrect() method
+
+}
 			
 		if (question1.isShowing()) {
 			// 3. If they selected the right fruit, do steps 4 and 7
+			if (keyCode==67) {
+				
 			
 				// 4. Call the correct() method
-				
+				correct();
 				// 7. Use the nextQuestion() method to go to question2
-			
-			
+			nextQuestion(question2);
+			}
 			// 8. else (if they touched something else)
-				
+			else  {
+			incorrect();	
+			} 
 				// 9. Call the incorrect() method
 		
 		}
@@ -49,16 +111,17 @@ public class FruitQuiz extends KeyAdapter {
 
 	private void correct() {
 		// 5. Find a sound for when they get a question right, and drag it into
+		
 		// the default package. It must be a .wav file. 
 		// There are lots on freesound.org
 		// 6. Use the playSound method to play your sound
-
+playSound("correct.wav");
 
 	}
 
 	private void incorrect() {
 		// 10. Find a sound for wrong answers and put it in the default package. Use the playSound method to play it.
-
+		playSound("wrong.wav");
 	}
 
 	private void nextQuestion(JLabel newQuestion) {
